1557acafdda03e718f5a001c472cf3b456b8f4ca
itagaki <itagaki.shintaro@gmail.com>
2015-02-06 12:44:49 +0900

    Test github webhook

--- deploy.php
+++ deploy.php
@@ -1,2 +1,6 @@
 <?php
-echo exec('/bin/sh deploy.sh');
+$data = json_decode($_POST['payload'],true);
+$branch = $data['ref'];
+if ($branch == 'refs/heads/master') {
+  exec('cd /var/www/onreco;git pull origin master > /dev/null 2>&1 &');
+}
